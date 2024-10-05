94753a6a86beec010eb28d99cffd44c1a5c0a6c6
Masky <biloxwolf@gmail.com>
2024-10-05 18:55:40 +0200

    changed language to english

--- Assets/Scripts/UiControllers/MenuButtonsController.cs
+++ Assets/Scripts/UiControllers/MenuButtonsController.cs
@@ -6,18 +6,18 @@ public class MenuButtonsController : MonoBehaviour
 {
     [SerializeField] Canvas CanvasToShow;
     [SerializeField] Canvas CanvasToHide1;
-    [SerializeField] Canvas CanvasToHide2;
+    //[SerializeField] Canvas CanvasToHide2;
     public void ChangeCanvas()
     {
 
         CanvasToHide1.sortingOrder = 0;
-        CanvasToHide2.sortingOrder = 0;
+        //CanvasToHide2.sortingOrder = 0;
 
         CanvasToHide1.enabled = false;
-        CanvasToHide2.enabled = false;
+        //CanvasToHide2.enabled = false;
 
         CanvasToHide1.renderMode = RenderMode.WorldSpace;
-        CanvasToHide2.renderMode = RenderMode.WorldSpace;
+        //CanvasToHide2.renderMode = RenderMode.WorldSpace;
 
         CanvasToShow.sortingOrder = 1;
 
